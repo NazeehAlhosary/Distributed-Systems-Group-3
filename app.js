8d516c499faf35b58189032cdef78e466ea3da0b
Nazeeh-Alhosary <gusalhmn@student.gu.se>
2018-11-21 20:59:37 +0100

    Fix JS and CSS files paths merge wih Ranim's work for the home page merge with Majed, Mohammad and Majd's works for Convert code From XML to JSON Put the file's name in the Database as the selected folder name

--- app.js
+++ app.js
@@ -3,13 +3,31 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 const app = express();
+var port = 1515;
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 // init the all files in the folder controllers
-//app.use('controllers', express.static(path.join(__dirname,'controllers')));
+///app.use('controllers', express.static(path.join(__dirname,'controllers')));
+app.use(express.static((__dirname,'controllers')));
+
+
+
 app.get('/', (req,res) => {
+
+    res.sendFile(path.join(__dirname , 'controllers', 'indexRan.html'));
+});
+
+
+app.get('/test', (req,res) => {
+
+    res.sendFile(path.join(__dirname , 'controllers', 'indexRan.html'));
+});
+
+
+
+app.get('/Projects', (req,res) => {
 
     res.sendFile(path.join(__dirname + '/controllers/MainPage.html'));
 });
@@ -19,23 +37,11 @@ app.get('/Classes', (req,res) => {
     res.sendFile(path.join(__dirname + '/controllers/index.html'));
 });
 
-//app.listen(3000,() => console.log("Server Started..,") );
-// Error handler (must be registered last)
-var env = app.get('env');
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    var err_res = {
-        "message": err.message,
-        "error": {}
-    };
-    if (env === 'development') {
-        err_res["error"] = err;
-    }
-    res.status(err.status || 500);
-    res.json(err_res);
-});
+app.listen(port,() =>{
+    console.log("Server Started on the PORT: " + port);
+    console.log("The server runs on: ((  http://localhost:" + port +" ))");
+} );
 
-app.listen(3000,() => console.log("Server Started..,") );
 
 
 module.exports = app;
---
+++ controllers/vendor/Myscriipt.js
@@ -0,0 +1,91 @@
+//String to save XML String
+var StringXML = "";
+
+/* Project Template (JSON objects) to save all interfaces + all classes + all relations
+for one project as one JSON object */
+
+var myProject = {
+  Interfaces: [],
+  Relations: [],
+  Classes: []
+};
+
+//Function that can be called in HTML input tag which takes the input file (XML)
+
+var MyFilter = function(event) {
+  var input = event.target;
+
+  /* FileReader is used to read the contents of a File
+check this https://www.javascripture.com/FileReader */
+
+  var reader = new FileReader();
+  var onload = function(event) {
+
+    //reader has many objects and we want the result object which has string (our xml string)
+    StringXML = reader.result;
+    //we use xmlToJSON library that we included as xmlToJSON.js in our script in HTML code ans use it here
+    var result = xmlToJSON.parseString(StringXML);
+    //Acording to srcML documentaion we extraxt first the array that has the objects that we need
+    myObject = result.unit[0].unit
+    //loop in our object array to extract intefaces + classes + realtions
+    for (var i = 0; i < myObject.length; i++) {
+      /* Try and catch importatnt to catch the error when we loop on an objec and there will be no object
+          with the name interface or class or extends (acording to srcML documentation) */
+      try {
+        /* save interface name into string (extract it from intreface.name.text ) and then add it to interface
+            array inside our myProject object that we created and same for realtions and classes */
+        myinterface = myObject[i].interface[0].name[0]._text;
+        myProject.Interfaces[i] = {
+          "InterfaceName": myinterface
+        }
+      } catch (e) {}
+      try {
+        myclass = myObject[i].class[0].name[0]._text;
+        mysuper = myObject[i].class[0].super[0].extends[0].name[0]._text;
+        myProject.Relations[i] = {
+          "ClassName": myclass,
+          "SuperClass": mysuper
+        }
+
+      } catch (e) {}
+
+      try {
+        myclass = myObject[i].class[0].name[0]._text;
+        myProject.Classes[i] = {
+          "ClassName": myclass
+        }
+
+      } catch (e) {}
+
+    }
+
+    console.log(myProject);
+  };
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/onload
+  reader.onload = onload;
+  reader.readAsText(input.files[0]);
+};
+
+//Function to print all project interfaces + classes + relations
+/*
+function PrintProject() {
+  console.log("Project Interfaces are :");
+  for (var i = 0; i < myProject.Interfaces.length; i++) {
+    var interface = myProject.Interfaces[i].InterfaceName;
+    console.log(interface);
+  }
+  console.log("Project Classes are :");
+  for (var i = 0; i < myProject.Classes.length; i++) {
+    var cname = myProject.Classes[i].ClassName;
+    console.log(cname);
+  }
+  console.log("Project Relations are :");
+  for (var i = 0; i < myProject.Relations.length; i++) {
+    var crname = myProject.Relations[i].ClassName;
+    var srname = myProject.Relations[i].SuperClass;
+    console.log(crname + " extends " + srname);
+  }
+
+}
+*/
