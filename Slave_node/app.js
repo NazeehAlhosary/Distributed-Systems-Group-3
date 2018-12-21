10513d43591b4d0b2ed7d2416f38987857237cb3
Mohammad Zreik <guszremo@student.gu.se>
2018-12-21 20:42:55 +0100

    Fixed multiple bugs. Cleared the project. Merged with front-end & back-end. Distribution works smoothly. Deletion works. Fault Tolerance applied and tested. Documented the code.
    Co-authored-by: M Nazeeh Alhosary <gusalhmn@student.gu.se>
    Co-authored-by: Majd Hatoum <majd.faris@hotmail.com>
    Co-authored-by: Ranim Khojah <guskhojra@student.gu.se>
    Co-authored-by: Majed Dalain <majed-dalain@hotmail.com>

--- Slave_node/app.js
+++ Slave_node/app.js
@@ -1,91 +1,138 @@
 var app = require('express')();
 var express = require('express');
-
 var server = require('http').Server(app);
 var socket = require('socket.io-client')('http://192.168.43.215:3001');
 var io = require('socket.io')(server);
 server.listen(4000);
 
+//Data received from Master node saved in globaldata (String) & globalObject (Object)
 var globaldata;
+var globalObject;
 
 
 app.use(express.static((__dirname, 'controllers')));
 app.set('appPath', 'controllers');
 
 
-
+//Default route
 app.get('/', function (req, res) {
-
-
   if (globaldata !== undefined) {
-    res.sendfile('controllers/radialTree.html');
-   }
-   else{
-      res.sendfile('controllers/home.html');
-   }
+
+    res.sendfile('controllers/radialTreeHandler.html');
+  }
+  else {
+    res.sendfile('controllers/home.html');
+  }
 
 });
 
+//Routes
 app.get('/v1', function (req, res) {
-  res.sendfile('controllers/tree.html');
+  res.sendfile('controllers/radialTree.html');
 });
 app.get('/v2', function (req, res) {
   res.sendfile('controllers/sunBurst.html');
 });
+app.get('/v3', function (req, res) {
+  res.sendfile('controllers/tree.html');
+});
+app.get('/v4', function (req, res) {
+  res.sendfile('controllers/sunBurstHandler.html');
+});
+app.get('/v5', function (req, res) {
+  res.sendfile('controllers/treeHandler.html');
+});
 
 
+//Sync with Master node to get Data
 socket.on('fromMaster', function (data) {
 
-
-  if (data !== undefined && data !==null) {    
+  if (data !== undefined && data !== null) {
     globaldata = data;
-
-    fromMasterMo();
+    DataToHome();
   }
-  else{
 
-  
-    
-  }
-  
- 
 });
 
-var globalObject;
+//Sync with Master node to get Data (After Delete)
+socket.on('fromMasterDelete', function (data) {
+
+  if (data !== undefined && data !== null) {
+    globaldata = data;
+    DataToHome();
+    refresh();
+  }
+});
+
+
+
+
 io.on('connection', function (socket) {
-  socket.on('fromHome', function (data,data2) {
 
-    
-    globaldata = data ;
+  socket.on('fromHome', function (data, data2) {
+
+    globaldata = data;
     globalObject = data2;
-    sendToMaster();
+
+    Uploaded_DataToMaster();
 
   });
- 
-  socket.on('data', function (data) {
-    fromMasterMoh(data);
 
-    
+
+  socket.on('deletedData', function (data) {
+
+    Deleted_DataToMaster(data);
 
   });
+
+
+  Update_DataForHandler();
 
 });
 
-function sendToMaster() {
-  socket.emit('fromSlave', globaldata,globalObject);
-}
-function fromMasterMo() {
-  io.emit('fromSlaveToHome',globaldata);
-<<<<<<< HEAD
-}
-=======
-}
 
-function fromMasterMoh(data) {
-  socket.emit('deleteClass',data);
+
+
+//Functions for sending new uploaded data and Deleted data to Master node
+
+function Uploaded_DataToMaster() {
+  socket.emit('fromSlave', globaldata, globalObject);
 }
 
+function Deleted_DataToMaster(data) {
+  socket.emit('deleteClass', data);
+}
+
+
+//Function for sending data to Home.html 
+
+function DataToHome() {
+  io.emit('fromSlaveToHome', globaldata);
+}
+
+
+//Function for sending an order to SunBurst.html & tree.html & radialTree.html to refresh the page 
+
+function refresh() {
+  io.emit('ref');
+}
+
+//Function for updating the handlers with new chamges to save it in Local storage
+function Update_DataForHandler() {
+  io.emit('T1', globaldata);
+}
 
 
 
->>>>>>> f7a81db1c7c09c6c9cddf4ff1dc2c355b8cfc397
+
+
+
+
+
+
+
+
+
+
+
+
